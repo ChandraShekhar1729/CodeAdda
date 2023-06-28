7a550c24e7922409ec7f324fa703c21bdfce25ec
Chandra Shekhar <100680681+ChandraShekhar1729@users.noreply.github.com>
2023-06-29 00:48:37 +0530

    Added solution - LeetHub

--- Bubble Sort - GFG/bubble-sort.java
+++ Bubble Sort - GFG/bubble-sort.java
@@ -14,13 +14,17 @@ class Solution
 	public static void bubbleSort(int arr[], int n)
     {
        for( int i=0;i<arr.length-1;i++){
-           
+           boolean swapped=false;
            for(int j=0;j<n-i-1;j++){
                if(arr[j]>arr[j+1]){
                     int temp=arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
+                    swapped=true;
                }
+           }
+           if(swapped==false){
+               break;
            }
        }
     }
